3ac4866a69f5930ad528427cbb2e3da2cf1d4853
EdenLinux <allanvoide@gmail.com>
2026-07-09 22:56:50 -0300

    fix: enabing cors middleware

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"backend/sezzle_test/internal/calculator/routes"
+	"backend/sezzle_test/internal/middleware"
 )
 
 func main() {
 
 	router := gin.Default()
+	router.Use(middleware.CORSMiddleware())
 
 	routes.RegisterRoutes(router)
 

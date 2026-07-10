6166120f696873ebfc1e9f222f040c91bbdba37e
EdenLinux <allanvoide@gmail.com>
2026-07-10 01:45:02 -0300

    docs: swagger documentation

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
+	_ "backend/sezzle_test/docs"
 	"backend/sezzle_test/internal/calculator/routes"
 	"backend/sezzle_test/internal/middleware"
 )
+
+//	@title			Sezzle Calculator API
+//	@version		1.0
+//	@description	Calculation API for the Sezzle calculator.
+//	@host			localhost:8080
+//	@BasePath		/
+//	@schemes		http
 
 func main() {
 
@@ -13,6 +23,8 @@ func main() {
 	router.Use(middleware.CORSMiddleware())
 
 	routes.RegisterRoutes(router)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run(":8080")
 }
